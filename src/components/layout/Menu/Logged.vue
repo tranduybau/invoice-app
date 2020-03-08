853560a66d7e3ee4d78074f046253ad7d9f553e6
trdbau <trdbau@gmail.com>
2020-03-08 17:49:10 +0700

    add layout home and fix nav, learn about beforeRouteEnter

--- src/components/layout/Menu/Logged.vue
+++ src/components/layout/Menu/Logged.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="inline-flex">
-    loggin
+  <div class="inline-flex justify-end h-full items-center">
+    <div class="mr-3 cursor-pointer">
+      <BellRing v-if="isHasNotify" />
+      <Bell v-else />
+    </div>
+    <img src="@/assets/user.png" alt="" class="h-full" />
   </div>
 </template>
 
 <script>
+import Bell from 'vue-material-design-icons/BellOutline.vue';
+import BellRing from 'vue-material-design-icons/BellRingOutline.vue';
+
 export default {
   name: 'Logged',
+  data() {
+    return {
+      isHasNotify: false,
+    };
+  },
+  components: {
+    Bell,
+    BellRing,
+  },
 };
 </script>
---
+++ src/faker/listInvoices.js
@@ -0,0 +1,8 @@
+const listInvoices = [
+  {
+    id: 'bau-1',
+    shortId: '1',
+  },
+];
+
+export default listInvoices;
